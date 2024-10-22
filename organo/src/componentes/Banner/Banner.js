de6a8867287c327c789f63f40ec3cdf8a3beb147
Guilherme Soares Sousa Silva <guilherme.sousa009@hotmail.com>
2024-10-22 14:53:48 -0300

    Criando Formulario

--- organo/src/componentes/Banner/Banner.js
+++ organo/src/componentes/Banner/Banner.js
@@ -1,11 +1,9 @@
 import './Banner.css'
 
-function Banner() {
+export const Banner = () => {
     return (
         <header className="banner">
             <img src="/img/banner.png" alt="Banner princiapl com cores roxas da página do Organo"  />
         </header>
     );
 }
-
-export default Banner;
