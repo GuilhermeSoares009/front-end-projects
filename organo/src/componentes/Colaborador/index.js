acf494345618d97be415ea2407431d42279bebf8
Guilherme Soares Sousa Silva <guilherme.sousa009@hotmail.com>
2024-10-23 11:49:35 -0300

    Card de colaborador

---
+++ organo/src/componentes/Colaborador/index.js
@@ -0,0 +1,17 @@
+import './Colaborador.css'
+
+const Colaborador = () => {
+    return (
+        <div className='colaborador'>
+            <div className='cabecalho'>
+                <img src='https://github.com/GuilhermeSoares009.png' alt='Guilherme Soares' />
+            </div>
+            <div className='rodape'>
+                <h4>Guilherme Soares</h4>
+                <h5>Desenvolvedor Full Stack Júnior</h5>
+            </div>
+        </div>
+    )
+}
+
+export default Colaborador
