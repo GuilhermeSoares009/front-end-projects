68c1f3bcef2b5feea268253e4f19c3781e4b91ae
Guilherme Soares Sousa Silva <guilherme.sousa009@hotmail.com>
2024-10-23 09:06:56 -0300

    Cadastrando colaboradores

--- organo/src/App.js
+++ organo/src/App.js
@@ -1,12 +1,21 @@
 import './index.css';
 import Banner from './componentes/Banner';
 import Formulario from './componentes/Formulario';
+import { useState } from 'react';
 
 function App() {
+
+  const [colaboradores, setColaboradores] = useState([])
+
+  const aoNovoColaboradorAdicionado = (colaborador) => {
+    console.log(colaborador)
+    setColaboradores([...colaboradores,colaborador])
+  }
+
   return (
     <div className="App">
       <Banner/>
-      <Formulario/>
+      <Formulario aoColaboradorCadastrado={colaborador => aoNovoColaboradorAdicionado(colaborador)} />
     </div>
   );
 }
